1807f31b42be5ff137f3411b0679208f8ea036ab
김연호 <kimfine000@gmail.com>
2025-08-02 16:34:32 +0900

    Refactor main function to include sum calculation and grading logic

--- src/1.kt
+++ src/1.kt
@@ -1,14 +1,18 @@
-//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
-// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 fun main() {
-    val name = "Kotlin"
-    //TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
-    // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.
-    println("Hello, " + name + "!")
+    var a: Int = 10 // 변수 값 변경 ㄱㄴ
+    val b : Double = 3.14 // 값 변경 ㄴㄴ
+    val sum = a + b
 
-    for (i in 1..5) {
-        //TIP <shortcut actionId="Debug"/>을(를) 눌러 코드 디버그를 시작하세요. 1개의 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 중단점을 설정해 드렸습니다
-        // 언제든 <shortcut actionId="ToggleLineBreakpoint"/>을(를) 눌러 중단점을 더 추가할 수 있습니다.
-        println("i = $i")
+    if (sum > 10) {
+        println("sum이 10보다 큽니다.")
+    } else {
+        println("sum이 10 이하입니다.")
+    }
+
+    val grade = 90
+    when (grade) {
+        in 90..100 -> println("A")
+        in 80..89 -> println("B")
+        else -> println("C 이하")
     }
 }
